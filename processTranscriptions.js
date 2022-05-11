d8e1039d6ed385f01e736fea8e8532ff3d2bfe34
tarekfb <56593098+tarekfb@users.noreply.github.com>
2022-05-11 22:42:51 +0200

    Update processTranscriptions.js

--- processTranscriptions.js
+++ processTranscriptions.js
@@ -9,6 +9,15 @@ function getIndecesOfAllParticipants(lines) {
   return indeces;
 }
 
+function actuallyGetIndecesOfAllParticipants(lines) {
+  let re = /[0-9]./;
+  const indeces = [];
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].substring(0, 7).match(re)) indeces.push(i);
+  }
+  return indeces;
+}
+
 function extractInitialsFromLine(line) {
   return line.substring(line.length - 3).replace(/\s/g, '');
 }
@@ -42,42 +51,116 @@ function removeRepitions(string, lines) {
 }
 
 function addNumbers(lines) {
-  // add numbers
   let counter = 1;
   for (let index = 0; index < lines.length; index++) {
-    let substring = lines[index].substring(0, 2)
-    switch (substring) {
-      case "AO":
-      case "TB":
-      case "R1":
-      case "R2":
-      case "R3":
-      case "R4":
-        lines[index] = counter + ". " + lines[index];
-        counter++;
-      default:
-        break;
+    if (lines[index]) {
+      let substring = lines[index].substring(0, 2)
+      switch (substring) {
+        case "AO":
+        case "TB":
+        case "R1":
+        case "R2":
+        case "R3":
+        case "R4":
+          lines[index] = counter + ". " + lines[index];
+          counter++;
+        default:
+          break;
+      }
     }
   }
 
   return lines;
 }
 
-function processTranscription(string) {
-  let myLines = string.split('\n');
-  myLines = addNumbers(myLines);
- 
-  myLines = removeRepitions("TB", myLines);
-  myLines = removeRepitions("AO", myLines);
-  myLines = removeRepitions("R1", myLines);
-  myLines = removeRepitions("R2", myLines);
-  myLines = removeRepitions("R3", myLines);
-  myLines = removeRepitions("R4", myLines);
+function replaceInitials(lines) {
+  let speaker1 = prompt("What is the initials of speaker 1?");
+  let speaker2 = prompt("What is the initials of speaker 2?");
+  let speaker3 = prompt("What is the initials of speaker 3?");
+
+  for (let index = 0; index < lines.length; index++) {
+    let substring = lines[index].substring(1, 8)
+    // console.log(lines[index].substring(1, 9))
+    // console.log(substring == "Speaker")
+    // console.log(substring.length);
+    // console.log("Speaker".length)
+    if (substring === "Speaker") {
+      let full = lines[index].substring(1, 10)
+      switch (full) {
+        case "Speaker 1":
+          lines[index] = speaker1
+          break;
+        case "Speaker 2":
+          lines[index] = speaker2
+          break;
+        case "Speaker 3":
+          lines[index] = speaker3
+          break;
+        case "Speaker 4":
+          alert("Found a 4th speaker at line: " + lines[index + 2] + "\nPlease remove any instances of a 4th speaker or this might not work.")
+          break;
+        default:
+          break;
+      }
+    }
+  }
+  return lines;
+}
+
+function removeNumbers(lines) {
+  let allNumberedLines = actuallyGetIndecesOfAllParticipants(lines);
+  console.log(allNumberedLines)
+  allNumberedLines.forEach(indexOfNumberedLine => {
+    const removeFrom = lines[indexOfNumberedLine].indexOf(".") + 2;
+    lines[indexOfNumberedLine] = lines[indexOfNumberedLine].substring(removeFrom);
+  });
+  return lines;
+}
+
+function addNumbersAndRemoveBlanks(lines){
+  let counter = 1;
+  for (let index = 0; index < lines.length; index++) {
+    if (lines[index]) {
+      let substring = lines[index].substring(0, 2)
+      switch (substring) {
+        case "AO":
+        case "TB":
+        case "R1":
+        case "R2":
+        case "R3":
+        case "R4":
+          lines[index] = counter + ". " + lines[index];
+          lines.splice([index + 1], 1)
+          counter++;
+        default:
+          break;
+      }
+    }
+  }
+
+  return lines;
+}
+
+function processTranscription(lines) {
+  lines = replaceInitials(lines)
+  lines = addNumbers(lines);
+
+  lines = removeRepitions("TB", lines);
+  lines = removeRepitions("AO", lines);
+  lines = removeRepitions("R1", lines);
+  lines = removeRepitions("R2", lines);
+  lines = removeRepitions("R3", lines);
+  lines = removeRepitions("R4", lines);
+
+  lines = removeNumbers(lines);
+  lines = addNumbersAndRemoveBlanks(lines);
   
+
   const myDiv = document.getElementById("my-div");
-  let processedTranscription = myLines.join('\n')
+  let processedTranscription = lines.join('\n')
   myDiv.innerHTML = processedTranscription;
 }
 
 const myTranscription = `add a transcription here that has been transcribed by word online`
+let lines = r2v2.split('\n');
 processTranscription(myTranscription);
